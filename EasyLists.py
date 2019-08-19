250c728e96ced5a212126beaa611e2d1f1497c6b
Callum Say <54273032+CallumSay@users.noreply.github.com>
2019-08-19 13:14:02 +0100

    Add files via upload

---
+++ EasyLists.py
@@ -0,0 +1,9 @@
+
+num_list = [1,5,23,76,12,3,9,48,66,31]
+
+sorted_list = list(filter(lambda x: x>30, num_list))
+print(sorted_list)
+
+#for item in num_list:
+ #   if(item > 30):
+  #      print(item)
---
+++ Intro.py
@@ -0,0 +1,8 @@
+
+a = int(input("Enter a number: "))
+
+if(a % 2 == 0):
+    print("your number is even")
+else:
+    print("your number is odd")
+
