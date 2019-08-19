741c500c0a2c567e115ff30fe96486348292db71
Callum Say <54273032+CallumSay@users.noreply.github.com>
2019-08-19 13:49:46 +0100

    Add files via upload

---
+++ Divisors.py
@@ -0,0 +1,8 @@
+
+userInput = int(input("Enter a number: "))
+numList = range(2,userInput)
+divisorList = []
+for item in numList:
+    if(userInput%item == 0):
+        divisorList.append(item)
+print(divisorList)    
