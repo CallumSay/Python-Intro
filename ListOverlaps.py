a980cb903cb240da339d3c3a42b7425c154e7b04
Callum Say <54273032+CallumSay@users.noreply.github.com>
2019-08-20 17:05:33 +0100

    Update ListOverlaps.py

--- ListOverlaps.py
+++ ListOverlaps.py
@@ -4,7 +4,7 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 overlapList= []
 
 for item in a:
-    if item in b:
+    if item in b and item not in overlapList:
         overlapList.append(item)
 
 print("These are the numbers that overlap: ", overlapList)
